fc04eb0db7fe04781032d555b7a2cb6f300bcd48
hayes <admin@hayesmarketingfirm.com>
2020-05-31 17:57:19 -0500

    sections folder added to copy

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,7 +1,19 @@
 /*================ Styles ================*/
 import '../../styles/theme.scss';
+import '../../styles/theme.scss.liquid';
 
 /*================ Scripts ===============*/
+
+// Lazysizes
+import 'lazysizes/plugins/object-fit/ls.object-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes';
+import 'lazysizes/plugins/respimg/ls.respimg';
+import 'lazysizes/plugins/blur-up/ls.blur-up';
+
+// jQuery
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;

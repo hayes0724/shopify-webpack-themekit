d6f1adb36831d24c6e309adbad04570155148b78
hayes <admin@hayesmarketingfirm.com>
2020-06-02 11:36:14 -0500

    webpack config for dev

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    themes: {
+        development: {
+            id: '74500046908',
+            password: 'ebd6ce7f27aae8cdafb883a8a5b887b9',
+            store: 'hm-themekit.myshopify.com',
+            ignore: [
+                'settings_data.json' // leave this here to avoid overriding theme settings on sync
+            ]
+        },
+    }
+};

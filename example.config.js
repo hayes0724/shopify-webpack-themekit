027f8c07f753588bb1fccec6cc47e81136d11323
hayes <admin@hayesmarketingfirm.com>
2020-06-11 17:41:56 -0500

    dev server and syncing

--- example.config.js
+++ example.config.js
@@ -2,21 +2,21 @@
  *
  * @type {{network: {ipAddress: string, external: boolean}, themes: {development: {id: string, password: string, store: string, ignore: string[]}}}}
  * Network
- * - ipAddress: force ip to use for dev server {'null', 'localhost', '192.168.1.1'}
+ * - ipAddress: force ip to use for dev server {'null', 'localhost', 'ip address'}
  * - external: if no ip is provided use first external address
  * - interface: use a specific network interface, otherwise first interface is selected
  */
 module.exports = {
     network: {
-        ipAddress: null,
+        ipAddress: 'localhost',
         external: false,
-        interface: 'Ethernet',
+        interface: false,
     },
     themes: {
         development: {
-            id: '74500046908',
-            password: 'ebd6ce7f27aae8cdafb883a8a5b887b9',
-            store: 'hm-themekit.myshopify.com',
+            id: '74500024251', // Theme ID
+            password: 'ebd6ce7f27aae8cdafb883a456456456', // Private app password
+            store: 'mystore.myshopify.com', // Store URL
             ignore: [
                 'settings_data.json' // leave this here to avoid overriding theme settings on sync
             ]
